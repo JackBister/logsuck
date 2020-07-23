d380fab8a6f210e54c45b061048439c8fb78ab04
Jack Bister <jack@jackbister.com>
2020-07-23 23:29:51 +0200

    Fix(?) CI
    Turns out I'm actually an idiot

--- cmd/packager/main.go
+++ cmd/packager/main.go
@@ -3,20 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/shurcooL/vfsgen"
 )
 
 func main() {
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Println("error getting working directory:", err)
-	} else {
-		log.Println("working directory:", wd)
-	}
 	static := http.Dir("../../web/static/dist")
-	err = vfsgen.Generate(static, vfsgen.Options{
+	err := vfsgen.Generate(static, vfsgen.Options{
 		PackageName:  "web",
 		VariableName: "Assets",
 		Filename:     "../../internal/web/generated_assets.go",
