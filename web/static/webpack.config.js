9d4dc6f408b1b44627df4df74846d749240069fb
Jack Bister <jack@jackbister.com>
2020-08-07 22:22:23 +0200

    Bundle Bootstrap CSS/JS into the executable
    This allows the web GUI to run on computers that are not connected to
    the internet or if various CDNs are down.

--- web/static/webpack.config.js
+++ web/static/webpack.config.js
@@ -1,25 +1,39 @@
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    devtool: 'source-map',
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: './index.html', to: './dist/index.html' },
-                { from: './style.css', to: './dist/style.css' }
-            ]
-        })
-    ],
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: 'ts-loader' }
-        ]
-    },
-    output: {
-        path: __dirname,
-        filename: 'dist/app.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    }
+  devtool: "source-map",
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "./index.html", to: "./dist/index.html" },
+        { from: "./style.css", to: "./dist/style.css" },
+        {
+          from: "./node_modules/bootstrap/dist/css/bootstrap.min.css",
+          to: "./dist/bootstrap.min.css",
+        },
+        {
+          from: "./node_modules/bootstrap/dist/js/bootstrap.min.js",
+          to: "./dist/bootstrap.min.js",
+        },
+        {
+          from: "./node_modules/popper.js/dist/umd/popper.min.js",
+          to: "./dist/popper.min.js",
+        },
+        {
+          from: "./node_modules/jquery/dist/jquery.min.js",
+          to: "./dist/jquery.min.js",
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
+  },
+  output: {
+    path: __dirname,
+    filename: "dist/app.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
 };
